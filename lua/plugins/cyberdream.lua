a41ff8f003d23806ccb156b228c87f87ed29c182
GoodCheese92 <dltjdwls555@gmail.com>
2024-05-17 18:03:52 +0900

    tst

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -3,12 +3,14 @@ return {
   name = "cyberdream",
   opts = {
     transparent = true,
+    borderless_telescope = true,
     theme = {
       variant = "default",
       highlights = {
         Comment = { fg = "#A6A6A6", italic = true },
         -- sdoishdsdih
         Operator = { fg = "#B5B2FF" },
+        TelescopeNormal = { bg = "#FFFFFF" },
       },
     },
   },
